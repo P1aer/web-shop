2f317a790f479d8d20f543f580cbc20e94cf6062
P1aer <arhoangel@bk.ru>
2021-07-18 18:37:41 +0300

    feat: made backend register and login

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose")
+
+const app = express()
+
+app.use("/api/auth",require("./routes/auth.route"))
+
+
+
+const PORT = process.env.PORT || 5000
+
+async function start() {
+    try {
+        await mongoose.connect("mongodb+srv://admin:EFh91M11qgkSjk3N@cluster0.klqk9.mongodb.net/test?retryWrites=true&w=majority",{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            }
+        )
+        app.listen(PORT, () => console.log("Started"))
+    } catch (e) {
+        process.exit(1)
+    }
+
+}
+
+start()
+
+
+
---
+++ routes/auth.route.js
@@ -0,0 +1,71 @@
+const { Router } = require("express")
+const User = require("/modules/user")
+const { check, validationResult } = require("express-validator")
+const  jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const router = Router();
+
+// /api/auth/register
+router.post("/register", [
+    check("email", "Некорректный email").isEmail(),
+    check("password", "Некорректный пароль").isLength({ min: 5})
+], async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Некорректные данные"
+            })
+        }
+        const { email, password } = req.body
+        const check = await User.findOne({ email })
+        if (check) {
+            res.status(400).json({ message: "пользователь уже сущестует" })
+            return
+        }
+        const hashPassword = await bcrypt.hash(password, 12)
+        const user = new User ({ email, password: hashPassword })
+
+        await  user.save()
+        res.status(201).json({ message: "пользователь создан"})
+    } catch (e) {
+        res.status(500).json({ message: "Ошибка" })
+    }
+})
+
+// /api/auth/login
+router.post("/login", [
+    check("email", "Некорректный email").normalizeEmail().isEmail(),
+    check("password", "Некорректный пароль").exists()
+], async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Некорректные данные"
+            })
+        }
+        const { email, password } = req.body
+        const user = await User.findOne() ({ email })
+        if (!user) {
+            return res.status(400).json({ message: "Пользователя не существует"})
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ message: "Пароль не подходит"})
+        }
+        const token = jwt.sign(
+        { userId: user.id },
+        "Sample Text",
+            { expiresIn: "1hr" }
+        )
+        res.json( { token, userId: user.id })
+    } catch (e) {
+        res.status(500).json({ message: "Ошибка" })
+    }
+})
+
+
+module.exports = router
